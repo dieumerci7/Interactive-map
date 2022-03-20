ce437c870fc009e56d7ecb5cf9d00bb5f006c199
philosoph10 <77683432+philosoph10@users.noreply.github.com>
2022-03-20 18:53:00 +0200

    Template of the project
    Basic web page

---
+++ changeMaps.js
@@ -0,0 +1,8 @@
+$('#my-map').prepend('<img id="theImg" src="author.jpg" />')
+
+$('#dropdown').change(function () {
+    let val = $('#dropdown').val()
+    if (val === 'author') {
+
+    }
+})
---
+++ server.js
@@ -0,0 +1,9 @@
+let http = require('http');
+let fs = require('fs');
+http.createServer(function (req, res) {
+    fs.readFile('main.html', function(err, data) {
+        //res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
